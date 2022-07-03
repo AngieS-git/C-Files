1045277eb0dbce3a064858061cf14b8160e04c2c
AngieS-git <m.angelinesamson@gmail.com>
2022-07-03 20:11:06 +0800

    added properties to Vertex3d class

--- Fundamentals/3D_Coordinates.cs
+++ Fundamentals/3D_Coordinates.cs
@@ -7,6 +7,30 @@ namespace ClassSample
 {
   public class Vertex3d
   {
-
+    //fields
+    private double _x;
+    private double _y;
+    private double _z;
+    //properties
+    public double X
+    {
+      get {return _x;}
+      set {_x = value;}
+    }
+    public double Y
+    {
+      get {return _y;}
+      set {_y = value;}
+    }
+    public double Z
+    {
+      get {return _z;}
+      set {_z = value;}
+    }
+    //method
+    public void SetToOrigin()
+    {
+      X = Y = Z = 0.0;
+    }
   }
 }
